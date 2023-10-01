5ea1e6fb047aae38854fae6c8987ff0cb2642d68
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-10-01 16:02:14 -0400

    add cross off and delete item features

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+let submitButton = $("#submit");
+
+//create list element
+submitButton.on("click", function (event) {
+  event.preventDefault();
+
+  //create and append elements
+  let listElement = $("#unordered-list");
+  let input = $("#input");
+  let inputValue = input.val();
+  let listItem = $("<li></li>");
+  listElement.append(listItem);
+  let itemFlex = $("<div></div>");
+  itemFlex.addClass("item-flex");
+  listItem.append(itemFlex);
+  itemFlex.append('<p class="item-left">' + inputValue + "</p>");
+  let itemLeft = $(".item-left");
+  itemFlex.append("<p class='close'>x</p>");
+
+  //cross item off the list
+  listItem.on("click", function () {
+    itemLeft.addClass("done");
+  });
+  //delete list item
+  let close = $(".close");
+  close.on("click", function () {
+    listItem.addClass("hidden");
+  });
+});
